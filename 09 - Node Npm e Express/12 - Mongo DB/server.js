acb80820e185f8ac41b7872edb2ae6f550a0c8ca
Nícolas Guedes <nicolastrocatti@gmail.com>
2025-05-31 23:30:59 -0300

    feat(Node, Npm and Express): adds a lesson about mongoose and dotenv

--- 09 - Node Npm e Express/12 - Mongo DB/server.js
+++ 09 - Node Npm e Express/12 - Mongo DB/server.js
@@ -1,6 +1,17 @@
 const express = require('express')
 const routes = require('./routes.js')
 const app = express();
+const mongoose = require('mongoose')
+
+require('dotenv').config()
+
+const connectionString = process.env.CONNECTIONSTRING
+mongoose.connect(connectionString)
+    .then(() => {
+        app.emit('pronto')
+    })
+    .catch(e => console.log(e))
+
 const path = require('path')
 const { middlewareGlobal } = require('./src/middlewares/middleware.js')
 
@@ -15,7 +26,10 @@ app.use(middlewareGlobal)
 
 app.use(routes);
 
-app.listen(3000, () => {
-    console.log('Acessar http://localhost:3000');
-    console.log('Sevidor executando na porta 3000');
-});
+app.on('pronto', () => {
+    app.listen(3000, () => {
+        console.log('Acessar http://localhost:3000');
+        console.log('Sevidor executando na porta 3000');
+    });
+})
+
---
+++ 09 - Node Npm e Express/12 - Mongo DB/src/controllers/home.controller.js
@@ -0,0 +1,18 @@
+const HomeModel = require('../models/HomeModel')
+
+HomeModel.create({
+    titulo:'Um Título de Testes',
+    descricao:'Uma descrição de testes.'
+})
+    .then(dados => console.log(dados))
+    .catch(e => console.log(e))
+
+exports.paginaInicial = (req, res, next) => {
+    res.render('index')
+    console.log(`Olha o que tem na req.session.nome ${req.session.nome}`)
+    next()
+}
+
+exports.trataPost = (req,res)=>{
+    res.send('Eu sou a sua rota de post')
+}
