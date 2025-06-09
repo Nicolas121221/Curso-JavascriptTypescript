419ff545316b739c9857107a15f42ad811f8bf3c
Nícolas Guedes <nicolastrocatti@gmail.com>
2025-06-08 23:08:53 -0300

    feat(Contact list project): adds the template for the project with some routes and pages already made

---
+++ 10 - Projeto Agenda/routes.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const router = express.Router()
+const homeController = require('./src/controllers/home.controller.js')
+const loginController = require('./src/controllers/login.controller.js')
+const contatoController = require('./src/controllers/contato.controller.js')
+const { loginRequired} = require('./src/middlewares/middleware.js')
+
+
+router.get('/', homeController.index)
+
+router.get('/login/', loginController.index)
+router.post('/login/register', loginController.register)
+router.post('/login/login', loginController.login)
+router.get('/login/logout', loginController.logout)
+
+router.get('/contato', loginRequired ,contatoController.index)
+router.post('/contato/register', loginRequired ,contatoController.register)
+router.get('/contato/:id', loginRequired ,contatoController.editIndex)
+router.post('/contato/edit/:id', loginRequired ,contatoController.edit)
+
+module.exports = router;
---
+++ 10 - Projeto Agenda/src/controllers/contato.controller.js
@@ -0,0 +1,63 @@
+const Contato = require('../models/contatoModel')
+
+exports.index = (req, res) => {
+    res.render('contato', {
+        contato: {}
+    })
+};
+
+exports.register = async (req, res) => {
+    try {
+        const contato = new Contato(req.body)
+        await contato.register();
+
+        if (contato.errors.length > 0) {
+            req.flash('errors', contato.errors);
+            req.session.save(() => res.redirect(`/contato`))
+            return
+        }
+
+        req.flash('success', 'Contato registrado com sucesso')
+        req.session.save(() => res.redirect(`/contato/${contato.contato.id}`))
+        return;
+    } catch (error) {
+        console.log(error);
+        return res.render('404')
+    }
+};
+
+exports.editIndex = async (req, res) => {
+    try {
+        if (!req.params.id) return res.render('404')
+
+        const contato = await Contato.buscaPorId(req.params.id)
+        if (!contato) return res.render('404')
+
+        res.render('contato', { contato })
+    } catch (error) {
+        console.log(error)
+        return res.render('404')
+    }
+};
+
+exports.edit = async (req,res)=>{
+        try {
+        if (!req.params.id) return res.render('404')
+        const contato = new Contato(req.body);
+        await contato.edit(req.params.id);
+
+            if (contato.errors.length > 0) {
+            req.flash('errors', contato.errors);
+            req.session.save(() => res.redirect('/contato'))
+            return
+        }
+
+        req.flash('success', 'Contato editado com sucesso')
+        req.session.save(() => res.redirect(`/contato/${contato.contato.id}`))
+        return;
+
+    } catch (error) {
+        console.log(error)
+        return res.render('404')
+    }
+}
