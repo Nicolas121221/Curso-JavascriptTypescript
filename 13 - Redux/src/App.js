a6537f09c3da07b9d3cbf7aa643a21eb7b986035
Nícolas Guedes <nicolastrocatti@gmail.com>
2025-07-31 23:56:08 -0300

    feat(Redux): adds a page to show the students in the api

--- 13 - Redux/src/App.js
+++ 13 - Redux/src/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import { Router } from 'react-router-dom/';
 import { ToastContainer } from 'react-toastify';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 
-
+import store, { persistor } from './store/index.js';
 import history from './services/history.js';
 import GlobalStyles from './styles/GlobalStyles.js';
 import Header from './components/header/index.js';
@@ -10,11 +12,18 @@ import Routes from './routes/index.js';
 
 export default function App() {
     return (
-        <Router history={history}>
-            <Header />
-            <Routes />
-            <GlobalStyles />
-            <ToastContainer autoClose={3000} className="toast-container" />
-        </Router>
+        <Provider store={store}>
+            <PersistGate persistor={persistor}>
+                <Router history={history}>
+                    <Header />
+                    <Routes />
+                    <GlobalStyles />
+                    <ToastContainer
+                        autoClose={3000}
+                        className="toast-container"
+                    />
+                </Router>
+            </PersistGate>
+        </Provider>
     );
 }
--- 13 - Redux/src/components/header/index.js
+++ 13 - Redux/src/components/header/index.js
@@ -1,16 +1,21 @@
 import React from 'react';
-import { Nav } from './styled';
-
+import { FaHome, FaSignInAlt, FaUserAlt } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
 
-import {FaHome, FaSignInAlt, FaUserAlt} from 'react-icons/fa'
+import { Nav } from './styled';
 
 export default function Header() {
     return (
         <Nav>
-            <Link to="/"><FaHome size={24}/></Link>
-            <Link to="/login"><FaUserAlt size={24} /></Link>
-            <Link to="#"><FaSignInAlt size={24} /></Link>
+            <Link to="/">
+                <FaHome size={24} />
+            </Link>
+            <Link to="/register">
+                <FaUserAlt size={24} />
+            </Link>
+            <Link to="/login">
+                <FaSignInAlt size={24} />
+            </Link>
         </Nav>
     );
 }
